42182c14adae20596d86c2b0ab6cd428d4185ab2
Erlend Geerts <erlen@lightning.local>
2023-09-25 19:17:15 +0200

    two lists (food and birds with fetch) with still hardcoded choice

--- js/functions.js
+++ js/functions.js
@@ -7,28 +7,33 @@ export const play = (id) => {
   audio.play();
 };
 
-export const dataSetLeft = [
-  "Apple",
-  "Cheese",
-  "Egg",
-  "Grapefruit",
-  "Hazelnut",
-  "Kiwi",
-  "Mango",
-  "Orange",
-  "Quiche",
-  "Strawberry",
-];
-
-export const dataSetRight = [
-  "Banana",
-  "Doughnut",
-  "Fig",
-  "Ice",
-  "Lemon",
-  "Nuts",
-  "Pizza",
-  "Rice",
-  "Tomato",
-  "Watermelon",
-];
+export const getDataSet = async (type) => {
+  if (type === "birds") {
+    const response = await fetch("http://ap-examen.surge.sh/vogels.json");
+    const data = await response.json();
+    return data.map((el) => el.latijnse_benaming);
+  } else {
+    return [
+      "Apple",
+      "Cheese",
+      "Egg",
+      "Grapefruit",
+      "Hazelnut",
+      "Kiwi",
+      "Mango",
+      "Orange",
+      "Quiche",
+      "Strawberry",
+      "Banana",
+      "Doughnut",
+      "Fig",
+      "Ice",
+      "Lemon",
+      "Nuts",
+      "Pizza",
+      "Rice",
+      "Tomato",
+      "Watermelon",
+    ];
+  }
+};
--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
 import "/css/style.scss";
-import { render, dataSetLeft, dataSetRight, play } from "./functions.js";
+import { render, play, getDataSet } from "./functions.js";
 
-const leftArray = dataSetLeft;
-const rightArray = dataSetRight;
 const leftList = document.querySelector(".app__left__list");
 const rightList = document.querySelector(".app__right__list");
 
-const main = () => {
+const main = async () => {
+  const dataSet = await getDataSet("birds");
+  const leftArray = dataSet.slice(0, Math.floor(dataSet.length / 2));
+  const rightArray = dataSet.slice(Math.floor(dataSet.length / 2));
   render(leftArray, leftList);
   render(rightArray, rightList);
-};
 
-leftList.onclick = (e) => {
-  leftArray.splice(leftArray.indexOf(e.target.innerHTML), 1);
-  rightArray.push(e.target.innerHTML);
-  play("audio-tic");
-  render(leftArray, leftList);
-  render(rightArray.sort(), rightList);
-};
+  leftList.onclick = (e) => {
+    leftArray.splice(leftArray.indexOf(e.target.innerHTML), 1);
+    rightArray.push(e.target.innerHTML);
+    play("audio-tic");
+    render(leftArray, leftList);
+    render(rightArray.sort(), rightList);
+  };
 
-rightList.onclick = (e) => {
-  rightArray.splice(rightArray.indexOf(e.target.innerHTML), 1);
-  leftArray.push(e.target.innerHTML);
-  play("audio-toc");
-  render(rightArray, rightList);
-  render(leftArray.sort(), leftList);
+  rightList.onclick = (e) => {
+    rightArray.splice(rightArray.indexOf(e.target.innerHTML), 1);
+    leftArray.push(e.target.innerHTML);
+    play("audio-toc");
+    render(rightArray, rightList);
+    render(leftArray.sort(), leftList);
+  };
 };
 
 main();
